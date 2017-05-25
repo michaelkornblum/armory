7c950cb2a24bb4cdf98ea0aa6b488d4c6667754f
michaelkornblum <mkornblum71@gmail.com>
2017-05-25 00:38:43 -0500

    add currency formating

--- src/components/itemListing/trArmory.vue
+++ src/components/itemListing/trArmory.vue
@@ -1,9 +1,14 @@
 <template>
   <tbody>
-    <tr v-for="item, index in items">
+    <tr v-for="(item, index) in items">
       <td>{{index + 1}}</td>
-      <td v-for="value in item">
-        {{value}}
+      <td v-for="(value, key) in item">
+        <span v-if="key == 'cost'">
+          <span v-if="value != 'N/A'">$</span> {{value | currency}}
+        </span>
+        <span v-else>
+          {{value}}
+        </span>
       </td>
       <td class="btn-group">
         <button class="btn btn-success">sell</button>
@@ -30,6 +35,15 @@
       bus.$on('tableChange', (payload) => {
         this.items = this.data[payload].items;
       });
+    },
+    filters: {
+      currency: (value) => {
+        if (typeof value === 'number') {
+          return value.toFixed(2);
+        }
+
+        return value;
+      },
     },
   };
 </script>
